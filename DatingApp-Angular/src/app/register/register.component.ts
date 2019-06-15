21b3c3030758f33fe85791adb6d90c4109af7f75
Oussail <oussail.alaoui@gmail.com>
2019-06-15 01:49:47 +0100

    added register component

---
+++ DatingApp-Angular/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(next => {
+      console.log('Registred Successfully');
+    }, error => {
+      console.log('Failed to register');
+    });
+  }
+
+  cancel(){
+    console.log('canceled');
+  }
+
+}
