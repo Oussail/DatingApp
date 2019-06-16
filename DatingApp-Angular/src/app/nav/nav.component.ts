a49ae4c3839ae2b841b3feb31fa2a482d3df81b3
Oussail <oussail.alaoui@gmail.com>
2019-06-16 15:41:46 +0100

    add our routes and route guard

--- DatingApp-Angular/src/app/nav/nav.component.ts
+++ DatingApp-Angular/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,7 +13,7 @@ export class NavComponent implements OnInit {
   model: any = {};
   token: any;
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
     // this.authService.decodeToken = this.authService.jwtHelper.decodeToken(localStorage.getItem('token'));
@@ -23,6 +24,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in Successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -33,6 +36,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged Out');
+    this.router.navigate(['/home']);
   }
 
 }
