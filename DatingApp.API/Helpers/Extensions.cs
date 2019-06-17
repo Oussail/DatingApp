42061349ff3696c06776f906df155a7912274c4c
Oussail <oussail.alaoui@gmail.com>
2019-06-18 00:47:04 +0100

    add calculate age function

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 namespace DatingApp.API.Helpers
 {
@@ -8,6 +9,15 @@ namespace DatingApp.API.Helpers
             reponse.Headers.Add("Application-Error", message);
             reponse.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             reponse.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static int CalculateAge(this DateTime theDateTime){
+            var age = DateTime.Today.Year - theDateTime.Year;
+            if (theDateTime.AddYears(age)> DateTime.Today)
+            {
+                age--;
+            }
+            return age;
         }
     }
 }
