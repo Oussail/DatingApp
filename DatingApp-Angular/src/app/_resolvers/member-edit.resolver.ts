0cbb261b9437d77745e2b0fb816c80918af2bebb
Oussail <oussail.alaoui@gmail.com>
2019-06-19 00:35:12 +0100

    adding memebr edit profile

---
+++ DatingApp-Angular/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { Observable, of } from 'rxjs';
+import { AlertifyService } from '../_services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User> {
+    constructor(private userService: UserService,
+                private authService: AuthService, private router: Router, private alertify: AlertifyService) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authService.decodeToken.nameid).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving your data');
+                this.router.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+
+}
