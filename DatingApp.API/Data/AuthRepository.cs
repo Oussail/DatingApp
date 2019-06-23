8d639801523329c4c9965d3af175b0e8b1092b1d
Oussail <oussail.alaoui@gmail.com>
2019-06-23 20:38:58 +0100

    including photos of logged in user

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -14,7 +14,7 @@ namespace DatingApp.API.Data
         }
         public async Task<User> Login (string username, string password)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Username == username);
             
             if(user == null)
                 return null;
