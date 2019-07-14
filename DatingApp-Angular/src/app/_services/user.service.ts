ab1305c2862f4e4fe225168ad1b9ecd8bee15573
Oussail <oussail.alaoui@gmail.com>
2019-07-14 01:44:11 +0100

    Adding the Mark as Read functionality

--- DatingApp-Angular/src/app/_services/user.service.ts
+++ DatingApp-Angular/src/app/_services/user.service.ts
@@ -109,4 +109,9 @@ export class UserService {
     return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id, {});
   }
 
+  markAsRead(userId: number, messageId: number) {
+    this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {})
+      .subscribe();
+  }
+
 }
--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -128,5 +128,23 @@ namespace DatingApp.API.Controllers
 
             throw new Exception("Error deleting the message");
         }
+
+        [HttpPost("{id}/read")]
+        public async Task<IActionResult> MarkMessageAsRead(int userId, int id)
+        {
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var message = await _repo.GetMessage(id);
+
+            if(message.RecipientId != userId)
+                return Unauthorized();
+            message.IsRead = true;
+            message.DateRead = DateTime.Now;
+
+            await _repo.SaveAll();
+
+            return NoContent();
+        }
     }
 }
